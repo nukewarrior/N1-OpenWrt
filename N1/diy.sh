ad1a984741a3b47feeef760e9e36c277545aeeb5
nukewarrior <journeyang@gmail.com>
2024-10-02 21:09:00 +0800

    update: 添加mosdns

--- N1/diy.sh
+++ N1/diy.sh
@@ -9,3 +9,6 @@ rm -rf feeds/luci/applications/luci-app-passwall
 # Add packages
 git clone https://github.com/nantayo/My-Pkg package/my-pkg
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+# mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
